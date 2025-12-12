43ec554b60f90afdb57cb359c5ed3e84af1bd077
follia <f.germani06@gmail.com>
2025-12-12 23:43:33 +0100

    Semplifica mod a solo client-side - rimuove codice server inutile

--- src/client/java/com/moddingminecraft/ModdingMinecraftClient.java
+++ src/client/java/com/moddingminecraft/ModdingMinecraftClient.java
@@ -1,10 +1,23 @@
 package com.moddingminecraft;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ModdingMinecraftClient implements ClientModInitializer {
+	public static final String MOD_ID = "moddingminecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("🎨 MOD STUPIDA CARICATA! Solo lato client - niente server!");
+		LOGGER.info("🎭 OGGETTO STUPIDO ATTIVATO! La tua mod ModdingMinecraft è caricata!");
+		LOGGER.info("🤪 Preparati per l'oggetto più stupido di Minecraft!");
+		LOGGER.info("💡 Questo oggetto non fa niente... ma è STUPIDO!");
+		LOGGER.info("🎪 Benvenuto nel mondo delle mod stupide!");
+
+		// 🤪 Qui ci sarà il nostro oggetto stupido!
+		// Per ora è solo un commento, ma presto sarà realtà!
 	}
 }

fb8b705538a7fdad7fd89581db77aa91e4c36233
follia <f.germani06@gmail.com>
2025-12-12 23:35:33 +0100

    working on starting

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,8 +1,8 @@
-package com.moddingminecraft;
+package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ModdingMinecraftClient implements ClientModInitializer {
+public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,13 +5,13 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModdingMinecraft implements ModInitializer {
-	public static final String MOD_ID = "moddingminecraft";
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "modid";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
@@ -20,5 +20,6 @@ public class ModdingMinecraft implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Ho aggiunto un oggetto stupido alla mod! 🎉");
 	}
 }

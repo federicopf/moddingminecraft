619fdf18c15458547ce61a4df904151be62ac1dd
follia <f.germani06@gmail.com>
2025-12-13 21:41:28 +0100

    working

--- src/main/java/com/moddingminecraft/ModdingMinecraft.java
+++ src/main/java/com/moddingminecraft/ModdingMinecraft.java
@@ -1,10 +1,10 @@
 package com.moddingminecraft;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +13,13 @@ public class ModdingMinecraft implements ModInitializer {
     public static final String MOD_ID = "moddingminecraft";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Item STUPID_ITEM = new Item(new Properties());
+    public static final Item STUPID_ITEM = new Item(new Item.Properties());
 
     @Override
     public void onInitialize() {
-        BuiltInRegistries.ITEM.register(
-                ResourceLocation.parse(MOD_ID + ":stupid_item"),
+        Registry.register(
+                BuiltInRegistries.ITEM,
+                Identifier.parse(MOD_ID + ":stupid_item"),
                 STUPID_ITEM
         );
 

86c369e132ad1a030afdd24239f5cab5bcc4c47f
follia <f.germani06@gmail.com>
2025-12-13 16:34:55 +0100

    working

--- src/main/java/com/moddingminecraft/ModdingMinecraft.java
+++ src/main/java/com/moddingminecraft/ModdingMinecraft.java
@@ -1,42 +1,27 @@
 package com.moddingminecraft;
 
-import com.moddingminecraft.item.StupidItem;
-
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModdingMinecraft implements ModInitializer {
-	public static final String MOD_ID = "moddingminecraft";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// Registrazione dell'item
-	public static final Item STUPID_ITEM = new StupidItem(new Item.Settings());
+    public static final String MOD_ID = "moddingminecraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// SERVER-SIDE: Logica di gioco, mob, fisica, mondo
-		LOGGER.info("🖥️ SERVER: ModdingMinecraft caricata lato SERVER!");
-		LOGGER.info("🎯 SERVER: Qui gestiamo la logica di gioco importante!");
-		LOGGER.info("🤖 SERVER: Mob, fisica, crafting, mondo - tutto qui!");
-		LOGGER.info("💾 SERVER: Salvataggio e sincronizzazione multiplayer!");
+    public static final Item STUPID_ITEM = new Item(new Properties());
 
-		// Registrazione dell'item
-		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "stupid_item"), STUPID_ITEM);
-		LOGGER.info("🎭 SERVER: L'Oggetto Stupidamente Stupido è registrato!");
-		LOGGER.info("🖼️ SERVER: Texture caricata: stupid_item.png");
-		LOGGER.info("📋 SERVER: Modello pronto: stupid_item.json");
-		LOGGER.info("🌍 SERVER: Traduzione italiana: 'Oggetto Stupidamente Stupido'");
+    @Override
+    public void onInitialize() {
+        BuiltInRegistries.ITEM.register(
+                ResourceLocation.parse(MOD_ID + ":stupid_item"),
+                STUPID_ITEM
+        );
 
-		// Qui aggiungeremo:
-		// - Nuovi blocchi
-		// - Nuove ricette crafting
-		// - Logica di gioco personalizzata
-		// - Eventi server-side
-	}
+        LOGGER.info("ModdingMinecraft loaded");
+    }
 }
--- src/main/java/com/moddingminecraft/ModdingMinecraftClient.java
+++ src/main/java/com/moddingminecraft/ModdingMinecraftClient.java
@@ -2,23 +2,13 @@ package com.moddingminecraft;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-
-import net.minecraft.item.ItemGroups;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class ModdingMinecraftClient implements ClientModInitializer {
-
-	@Override
-	public void onInitializeClient() {
-		// Usa il logger del mod principale
-		ModdingMinecraft.LOGGER.info("🖥️ CLIENT: ModdingMinecraft caricata lato CLIENT!");
-		// CLIENT-SIDE: Rendering, suoni, controlli, interfaccia utente
-		ModdingMinecraft.LOGGER.info("🎨 CLIENT: Qui gestiamo rendering, texture e suoni!");
-		ModdingMinecraft.LOGGER.info("🎮 CLIENT: Controlli personalizzati e interfaccia utente!");
-		ModdingMinecraft.LOGGER.info("🎵 CLIENT: Suoni e musica del mod!");
-
-		// Aggiungiamo l'item al gruppo creativo
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
-			entries.add(ModdingMinecraft.STUPID_ITEM);
-		});
-	}
+    @Override
+    public void onInitializeClient() {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> {
+            entries.accept(ModdingMinecraft.STUPID_ITEM);
+        });
+    }
 }

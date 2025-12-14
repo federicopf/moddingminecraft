d9c143d4be73dd7e4e3406236c0f68443a82a40c
follia <f.germani06@gmail.com>
2025-12-14 20:00:08 +0100

    block works

--- src/main/java/com/moddingminecraft/ModBlocks.java
+++ src/main/java/com/moddingminecraft/ModBlocks.java
@@ -3,19 +3,26 @@ package com.moddingminecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 
 public class ModBlocks {
-    public static final Identifier LIGHT_IRON_BLOCK_ID = Identifier.parse("moddingminecraft:light_iron");
-    public static final ResourceKey<Block> LIGHT_IRON_BLOCK_KEY = ResourceKey.create(Registries.BLOCK, LIGHT_IRON_BLOCK_ID);
 
-    public static final Block LIGHT_IRON_BLOCK = new Block(
-        BlockBehaviour.Properties.of()
-            .strength(0.5F) // veloce
-            .setId(LIGHT_IRON_BLOCK_KEY)
-    );
-    
-    private ModBlocks() {
+    public static Block LIGHT_IRON_BLOCK;
+
+    public static void register() {
+        Identifier id = Identifier.fromNamespaceAndPath(ModdingMinecraft.MOD_ID, "light_iron");
+        ResourceKey<Block> key = ResourceKey.create(Registries.BLOCK, id);
+
+        BlockBehaviour.Properties props = BlockBehaviour.Properties.of()
+            .mapColor(MapColor.METAL)
+            .strength(5.0F, 6.0F)
+            .requiresCorrectToolForDrops();
+
+        LIGHT_IRON_BLOCK = Blocks.register(key, props);
+        Items.registerBlock(LIGHT_IRON_BLOCK);
     }
 }
--- src/main/java/com/moddingminecraft/ModdingMinecraft.java
+++ src/main/java/com/moddingminecraft/ModdingMinecraft.java
@@ -1,13 +1,6 @@
 package com.moddingminecraft;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,13 +9,9 @@ public class ModdingMinecraft implements ModInitializer {
     public static final String MOD_ID = "moddingminecraft";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Block LIGHT_IRON_BLOCK = ModBlocks.LIGHT_IRON_BLOCK;
-    public static final Item LIGHT_IRON_ITEM = new BlockItem(LIGHT_IRON_BLOCK, new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ModBlocks.LIGHT_IRON_BLOCK_ID)));
-
     @Override
     public void onInitialize() {
-        Registry.register(BuiltInRegistries.BLOCK, ModBlocks.LIGHT_IRON_BLOCK_ID, LIGHT_IRON_BLOCK);
-        Registry.register(BuiltInRegistries.ITEM, ModBlocks.LIGHT_IRON_BLOCK_ID, LIGHT_IRON_ITEM);
+        ModBlocks.register();
         LOGGER.info("ModdingMinecraft loaded");
     }
 }

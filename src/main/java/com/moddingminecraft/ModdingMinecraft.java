40712256b072e0dd5d98c0ba312db9e385b591db
follia <f.germani06@gmail.com>
2025-12-26 01:10:24 +0100

    finished mod, needs refactoring

--- src/main/java/com/moddingminecraft/ModdingMinecraft.java
+++ src/main/java/com/moddingminecraft/ModdingMinecraft.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.moddingminecraft.registry.ItemRegistry;
 import com.moddingminecraft.ModCreativeTabs;
+import com.moddingminecraft.ModArmorEffects;
 
 public class ModdingMinecraft implements ModInitializer {
 
@@ -18,6 +19,8 @@ public class ModdingMinecraft implements ModInitializer {
         ModWorldGen.register();
         // Registra la creative tab DOPO gli item per assicurarsi che gli armor siano disponibili
         ModCreativeTabs.register();
+        // Inizializza il sistema degli effetti armor
+        ModArmorEffects.initialize();
         LOGGER.info("ModdingMinecraft loaded");
     }
 }
